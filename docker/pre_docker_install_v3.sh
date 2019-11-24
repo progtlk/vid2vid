797e7dba87e4d85544766d55ec8de9f6804b8106
progtlk <32945006+progtlk@users.noreply.github.com>
2019-11-24 12:24:53 +0800

    Update and rename pre_docker_install_v2.sh to pre_docker_install_v3.sh

--- docker/pre_docker_install_v3.sh
+++ docker/pre_docker_install_v3.sh
@@ -1,22 +1,9 @@
 #Thanks @dustinfreeman for providing the script
+#Modified by Wallen Mphepo to work on Win 10
 
 #Install docker-ce https://docs.docker.com/install/linux/docker-ce/ubuntu/#set-up-the-repository
 
 ######-----------COMMENTED OUT THIS BELOW SECTION SINCE DOCKER ALREADY INSTALLED ON MACHINE------------############
-#sudo apt-get install -y \
-#     apt-transport-https \
-#     ca-certificates \
-#     curl \
-#     software-properties-common
-
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-#sudo add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) \
-#   stable"
-#sudo apt-get update
-#sudo apt-get install -y docker-ce
 
 ######-----------COMMENTED OUT THIS ABOVE SECTION SINCE DOCKER ALREADY INSTALLED ON MACHINE------------############
 
